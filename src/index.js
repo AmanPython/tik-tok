ce0d42437c76ca48b9cb2597f27bd201bb6b41d4
niklaus18 <amangora1999@gmail.com>
2021-11-14 10:47:53 +0530

    ft: first arrow function

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ class Game extends React.Component{
 class Square extends React.Component{
     render(){
         return(
-            <button className="square" onClick={function(){console.log('click')}}>
+            <button className="square" onClick={console.log('click')}>
                 {this.props.value}
             </button>
         )
